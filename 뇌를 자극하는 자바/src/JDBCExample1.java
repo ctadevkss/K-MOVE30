3c50c8795f8ef9b7c4b523de6adffea600536650
Administrator <Administrator@c1-00>
2018-05-03 12:17:44 +0900

    JDBC - JDBCExample1

---
+++ 뇌를 자극하는 자바/src/JDBCExample1.java
@@ -0,0 +1,21 @@
+import java.sql.*;
+
+class JDBCExample1 {
+    public static void main(String args[]) {
+        Connection conn = null;
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            conn = DriverManager.getConnection(
+               "jdbc:mysql://localhost:3306/mysql", "root", "");
+            System.out.println("데이터베이스에 접속했습니다.");
+            conn.close();
+        }
+        catch (ClassNotFoundException cnfe) {
+            System.out.println("해당 클래스를 찾을 수 없습니다." + 
+                               cnfe.getMessage());
+        }
+        catch (SQLException se) {
+            System.out.println(se.getMessage());
+        }
+    }
+}
