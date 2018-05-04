67c2fc6a048b804bd9eb659a2d2b70d00abe2a0d
Administrator <Administrator@c1-00>
2018-05-04 09:51:46 +0900

    0504 - JDBC 주석추가

--- 뇌를 자극하는 자바/src/JDBCExample1.java
+++ 뇌를 자극하는 자바/src/JDBCExample1.java
@@ -1,13 +1,20 @@
-import java.sql.*;
+import java.sql.*;  // 패키지
 
 class JDBCExample1 {
     public static void main(String args[]) {
-        Connection conn = null;
-        try {
+    	
+    	// 1단계: DB 연결을 위한 커넥션 인터페이스 
+    	Connection conn = null;
+        // try ~ catch 문에서 DB연결중에 예외가 발생하는지를 검사.
+    	try {
+        	// 2단계: JDBC드라이버를 로드한다.
             Class.forName("com.mysql.jdbc.Driver");
+            // 3단계: 드라이버매니져 클래스는 getConnection메소드로 DB를 연결한다.
             conn = DriverManager.getConnection(
-               "jdbc:mysql://localhost:3306/mysql", "root", "");
+               "jdbc:mysql://localhost:3306/mysql", "root", "12345");
             System.out.println("데이터베이스에 접속했습니다.");
+            
+            // 4단계: DB연결을 종료한다.
             conn.close();
         }
         catch (ClassNotFoundException cnfe) {
