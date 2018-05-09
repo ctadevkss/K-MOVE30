bea65965ca0031cd00c5506900f0c63bf937fbbf
r1-21 <r1-21@r1-21-PC>
2018-05-09 12:21:39 +0900

    JDBC 0509

--- 뇌를 자극하는 자바/src/JDBC_Delete.java
+++ 뇌를 자극하는 자바/src/JDBC_Delete.java
@@ -32,8 +32,8 @@ class JDBC_Delete {
             // 4단계: DB연결을 종료한다.
             conn.close();
             
-            if(chageRecord > 0)
-            	System.out.println(chageRecord + "건이 삭제 되었습니다.");
+            if(changeRecord > 0)
+            	System.out.println(changeRecord + "건이 삭제 되었습니다.");
             else
             	System.out.println("삭제된 데이터는 없습니다.");
             
