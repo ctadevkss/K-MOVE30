47a984c03e0140139653ca2c1612a08cf13dfb72
Administrator <Administrator@c1-00>
2018-05-29 16:45:05 +0900

    GUI - 알림메시지 창

--- 뇌를 자극하는 자바/src/chap19/SelectActionListener.java
+++ 뇌를 자극하는 자바/src/chap19/SelectActionListener.java
@@ -3,7 +3,9 @@ package chap19;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 
+import javax.swing.JOptionPane;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
 
@@ -38,16 +40,37 @@ public class SelectActionListener implements ActionListener {
 		try {
 			rs = jdbcManager.SelectTable(query);
 			model.setNumRows(0);
-			while(rs.next()) {
+			
+			ResultSetMetaData md = rs.getMetaData() ;
+			int columnCount = md.getColumnCount();
+			System.out.println("컬럼갯수: " + columnCount);
+			
+			rs.last();  // 레코드셋에 커서를 마지막 행으로 이동
+
+			int rowCount = rs.getRow(); // 레코드 갯수  
+
+			rs.beforeFirst(); // 레코드셋에 커서를 처음 행으로 이동
+			
+			System.out.println("레코드 갯수: " + rowCount);
+			
+			// 조회된 결과가 1건이상 있다면 
+			if( rowCount >= 1 ) {
 				
-				arr[0] = rs.getString("pname");
-				arr[1] = rs.getString("age");
+				while(rs.next()) {
+					arr[0] = rs.getString("pname");
+					arr[1] = rs.getString("age");
+					//arr[2] = rs.getString("gender");
+				    // 성별을 남/여 구분값으로 변경
+				     arr[2] = rs.getString("gender").equals("m") ? "남": "여";
+					System.out.println(arr[0] + " " + arr[1] + " " + arr[2]);
+					model.addRow(arr); // 데이터를 테이블에 추가
+				}
+			} else {
+				// 조회된 결과가 없을때
 				
-				// 성별을 남/여 구분값으로 변경
-				arr[2] = rs.getString("gender").equals("m") ? "남": "여";
-				System.out.println(arr[0] + " " + arr[1] + " " + arr[2]);
-				model.addRow(arr); // 데이터를 테이블에 추가
+				JOptionPane.showMessageDialog(null, "조회된 결과가 없습니다.", "경고 메시지", JOptionPane.WARNING_MESSAGE);
 				
+				System.out.println("조회된 결과 없습니다.");
 			}
 			
 		}catch (Exception ex) {
@@ -57,9 +80,9 @@ public class SelectActionListener implements ActionListener {
 		//int row = table.getSelectedRow();
 		//int col = table.getSelectedColumn();
 		//Object value = table.getValueAt(row, col);
-		Object value = table.getValueAt(0, 1);
-		System.out.println((String)value);
+		//Object value = table.getValueAt(0, 1);
+		//System.out.println((String)value);
 
-		
+		System.out.println("메소드 종료");
 	}
 }
